14a285b2b73b91dfd623ce2005b1cc6b17ca927d
Pau <paulacagua29@hotmail.com>
2025-04-29 22:39:14 -0500

    Corregido ALLOWED_HOSTS con valor correcto

--- mobile11/settings.py
+++ mobile11/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-eg+a6^w7vqa(vb_*w$s_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [*] #host agregado 
+ALLOWED_HOSTS = ['*'] #host agregado 
 
 
 # Application definition
