8b22dea89c6d3fdf9732ea1e743ebb0e15eaf2c0
Pau <paulacagua29@hotmail.com>
2025-04-29 22:02:07 -0500

    Versión inicial para Render

--- mobile_fan/settings.py
+++ mobile_fan/settings.py
@@ -88,11 +88,12 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 
